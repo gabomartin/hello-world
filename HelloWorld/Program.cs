eac52519dcacecc265997c2f0642c3279f669375
gabomartin <75818899+gabomartin@users.noreply.github.com>
2022-05-23 19:08:30 -0300

    Modificacion literal.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!!!!");
+            Console.WriteLine("Hola lab!!!");
             Console.ReadLine();
         }
     }
