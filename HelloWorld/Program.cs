0193eb814d7ea318719b84626873b782b9d733a3
gabomartin <75818899+gabomartin@users.noreply.github.com>
2022-05-31 15:19:16 -0300

    Cambio literal.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    internal class Program
-    {
+     class Program
+     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola lab!!!");
+            float f = 2.1F;
+            int i = (int)Math.Ceiling(f);
+            Console.WriteLine("ORIGINAL " + f);
+            Console.WriteLine("REDONDEADO " + i);
+            
+
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(9);
+            list.Add(10);
+
+
+            List<List<int>> list2 = list.ChunkBy<int>(5); //el mejor extension method de la historia
+
+            foreach(List<int> item in list2)
+            {
+                Console.WriteLine("Tengo: " + item.Count());
+            }
+
             Console.ReadLine();
+
+
+
+
+
+
         }
-    }
+     }
+
 }
